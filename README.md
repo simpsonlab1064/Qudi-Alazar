48f1676adec8c64434e4efaa55a6d5a60610acac
Ben Cerjan <bcerjan@gmail.com>
2025-07-20 11:24:22 -0400

    Updated readme

--- README.md
+++ README.md
@@ -3,14 +3,6 @@
 
 ---
 
-A template repository to create your own pip-installable qudi namespace addon packages.
+A set of [Qudi modules](https://github.com/Ulm-IQO/qudi-core) for interfacing with Alazar
+cards. Replicates the data collection used by [Garth Simpson](https://www.chem.purdue.edu/simpson/)'s group at Purdue.
 
-Replace this README.md with your own projects' readme.
-
-> __WARNING:__
-> 
-> Do __NOT__ put any `__init__.py` files into qudi namespace packages. Doing so will prevent any 
-> addon packages to install additional modules into the respective package or any sub-packages.
-> 
-> You can however create your own non-namespace packages (including `__init__.py`). Just make sure 
-> you do not want to install any addons later on in this package or any sub-packages thereof.
--- setup.py
+++ setup.py
@@ -8,30 +8,30 @@ from setuptools import setup, find_namespace_packages
 # ONLY LIST DEPENDENCIES THAT ARE DIRECTLY USED BY THIS PACKAGE (no inherited dependencies from
 # e.g. qudi-core)
 unix_dep = [
-    'wheel>=0.37.0',
-    'qudi-core>=1.4.1',
-    'numpy>=1.21.3',
-    'pyqtgraph>=0.13.0',
-    'PySide2==5.15.2.1',
+    "wheel>=0.37.0",
+    "qudi-core>=1.4.1",
+    "numpy>=1.21.3",
+    "pyqtgraph>=0.13.0",
+    "PySide2==5.15.2.1",
 ]
 
 windows_dep = [
-    'wheel>=0.37.0',
-    'qudi-core>=1.4.1',
-    'numpy>=1.21.3',
-    'pyqtgraph>=0.13.0',
-    'PySide2==5.15.2.1',
+    "wheel>=0.37.0",
+    "qudi-core>=1.4.1",
+    "numpy>=1.21.3",
+    "pyqtgraph>=0.13.0",
+    "PySide2==5.15.2.1",
 ]
 
 # The version number of this package is derived from the content of the "VERSION" file located in
 # the repository root. Please refer to PEP 440 (https://www.python.org/dev/peps/pep-0440/) for
 # version number schemes to use.
-with open('VERSION', 'r') as file:
+with open("VERSION", "r") as file:
     version = file.read().strip()
 
 # The README.md file content is included in the package metadata as long description and will be
 # automatically shown as project description on the PyPI once you release it there.
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     long_description = file.read()
 
 # Please refer to https://docs.python.org/3/distutils/setupscript.html for documentation about the
@@ -47,27 +47,32 @@ with open('README.md', 'r') as file:
 # 6. Make sure your license tag matches the LICENSE (and maybe LICENSE.LESSER) file distributed
 #    with your package (default: GNU Lesser General Public License v3)
 setup(
-    name='qudi-addon-template',  # Choose a custom name
+    name="qudi-alazar",  # Choose a custom name
     version=version,  # Automatically deduced from "VERSION" file (see above)
-    packages=find_namespace_packages(where='src'),  # This should be enough for 95% of the use-cases
-    package_dir={'': 'src'},  # same
-    package_data={'': []},  # include data files
-    description='A template package for qudi addons.',  # Meaningful short(!) description
+    packages=find_namespace_packages(
+        where="src"
+    ),  # This should be enough for 95% of the use-cases
+    package_dir={"": "src"},  # same
+    package_data={"": []},  # include data files
+    description="Qudi modules for Alazar card data collection.",  # Meaningful short(!) description
     long_description=long_description,  # Detailed description is taken from "README.md" file
-    long_description_content_type='text/markdown',  # Content type of "README.md" file
-    url='https://github.com/Ulm-IQO/qudi-addon-template',  # URL pointing to your project page
-    keywords=['qudi',             # Add tags here to be easier found by searches, e.g. on PyPI
-              'experiment',
-              'measurement',
-              'framework',
-              'lab',
-              'laboratory',
-              'instrumentation',
-              'instrument',
-              'modular'
-              ],
-    license='LGPLv3',  # License tag
-    install_requires=windows_dep if sys.platform == 'win32' else unix_dep,  # package dependencies
-    python_requires='>=3.8, <3.11',  # Specify compatible Python versions
-    zip_safe=False
+    long_description_content_type="text/markdown",  # Content type of "README.md" file
+    # url="https://github.com/Ulm-IQO/qudi-addon-template",  # URL pointing to your project page
+    keywords=[
+        "qudi",  # Add tags here to be easier found by searches, e.g. on PyPI
+        "experiment",
+        "measurement",
+        "framework",
+        "lab",
+        "laboratory",
+        "instrumentation",
+        "instrument",
+        "modular",
+    ],
+    license="LGPLv3",  # License tag
+    install_requires=windows_dep
+    if sys.platform == "win32"
+    else unix_dep,  # package dependencies
+    python_requires=">=3.8, <3.11",  # Specify compatible Python versions
+    zip_safe=False,
 )
